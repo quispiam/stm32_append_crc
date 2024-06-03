164990616acda75cd0a8c6588e3b7e9baf45c136
Ferg <quispiam@gmail.com>
2024-06-03 13:39:50 +0000

    got the flake based compile working!

--- flake.nix
+++ flake.nix
@@ -5,36 +5,34 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
-    let
+  outputs = { self, nixpkgs }: let
 
-      system = "x86_64-linux";  # Change this if you're using a different architecture
-      pkgs = nixpkgs.legacyPackages.${system};
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ] (system: function nixpkgs.legacyPackages.${system});
+
     in {
 
-    #forAllSystems = function:
-    #  nixpkgs.lib.genAttrs [
-    #    "x86_64-linux"
-    #    "aarch64-linux"
-    #  ] (system: function nixpkgs.legacyPackages.${system});
-    #in {
+      packages = forAllSystems (pkgs: { 
+        stm32_append_crc = pkgs.stdenv.mkDerivation {
+          pname = "stm32_append_crc";
+          version = "1.0.0";  # You can change this to the actual version if known
 
-      #packages = forAllSystems (pkgs: { 
-        #default = pkgs.callPackage ./package.nix {};
-        pname = "stm32_append_crc";
-        version = "1.0.0";  # You can change this to the actual version if known
+          src = ./src/.;
 
-        src = ./.;
+          buildInputs = [ pkgs.gcc9 ];
 
-        buildInputs = [ pkgs.gcc pkgs.libstdcxx ];
+          buildPhase = ''
+            gcc -o stm32_append_crc *.cpp *.c -lstdc++ 
+          '';
 
-        buildPhase = ''
-          gcc -o stm32_append_crc *.cpp *.c -lstdc++
-        '';
-
-        installPhase = ''
-          mkdir -p $out/bin
-          cp stm32_append_crc $out/bin/
-        '';
-    };
+          installPhase = ''
+            mkdir -p $out/bin
+            cp stm32_append_crc $out/bin/
+          '';
+        };
+      });
+    };   
 }
